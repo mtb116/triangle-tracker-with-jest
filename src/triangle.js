8135d1836058e820e64a973ace122a7a731708a1
Franz Knupfer <franzknupfer@hotmail.com>
2019-09-23 10:49:01 -0700

    full project w jest

--- src/triangle.js
+++ src/triangle.js
@@ -1,22 +1,17 @@
 export function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
-  this.side3 = side3;
+  this.side3 = side2;
 }
 
 Triangle.prototype.checkType = function() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
-
-  }  else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
-    return "an equilateral triangle";
-
-  } else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
-    return "an isosceles triangle";
-
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
-    return "a scalene triangle";
-  } else {
-    return "unable to compute";
+    return "scalene triangle";
+  }  else if ((this.side1 === this.side2) && (this.side2 === this.side3)) {
+    return "equilateral triangle";
+  } else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+      return "isosceles triangle";
   }
 };
