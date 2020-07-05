5e8b3df3742fec3217fa17fd3dcb1d532a483a07
Cathy Bradley (ctb116) <catherinetybradley@gmail.com>
2020-07-04 20:07:37 -0700

    add comments to webpack.config file - what is webpack and entry points

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,45 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+//webpack is a module bundler
+//webpack loads assets, such as plugins, to run tasks such as concatenation and minification
+//webpack will load anything we need for our application (JavaScript, CSS, etc) as long as we have the right loaders and plugins
 
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+//webpack uses a dependency graph - recursively manage our application's assets
+//webpack needs an entry point to do this - to recursively gather all the files
+//entry point is like a door leading into our application - commonly called main.js
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist",
   },
   plugins: [
     new UglifyJsPlugin({ sourceMap: true }),
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new HtmlWebpackPlugin({
-      title: 'Triangle Tracker',
-      template: './src/index.html',
-      inject: 'body'
-    })
+      title: "Triangle Tracker",
+      template: "./src/index.html",
+      inject: "body",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
-        exclude: [
-                /node_modules/,
-                /spec/
-              ],
-        loader: "eslint-loader"
+        exclude: [/node_modules/, /spec/],
+        loader: "eslint-loader",
       },
-    ]
-  }
+    ],
+  },
 };
